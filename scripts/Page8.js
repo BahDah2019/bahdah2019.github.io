d1339dcbd56ddc1906baf51f3efe87243a388efa
danellep <danellepillay0@gmail.com>
2019-02-25 19:18:28 +0200

    last changes

--- scripts/Page8.js
+++ scripts/Page8.js
@@ -4,7 +4,7 @@ function myFunction() {
   x = document.getElementById("inputTextS").value;
   y = document.getElementById("inputTextE").value;
   
-  if ((x != "c") && (y!= "30.1796")) {
+  if ((x != "30.6924") && (y!= "30.1796")) {
     alert("You dont have to do much, just get the values from me...");
   document.getElementById("inputTextS").value = "";
   document.getElementById("inputTextE").value = "";
